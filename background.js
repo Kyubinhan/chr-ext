f53cf3eab4b9abc787510e9381476e876a1ee821
Kyubin Han <kyubinhan11@gmail.com>
2020-08-22 16:55:58 -0700

    Remove popup

--- background.js
+++ background.js
@@ -1,14 +1,45 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "" },
-          }),
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()],
-      },
-    ]);
+/*
+ * Window popup using page action
+ *
+ * @ref: https://developer.chrome.com/extensions/pageAction
+ * @ref: https://developer.chrome.com/extensions/examples/api/pageAction/set_icon.zip
+ * This is a workaround to make sure chrome.pageAction.onClicked works.
+ *
+ * The idea here to to force popup to show on GitHub and GitLab pages,
+ * but as we don't assign popup, it fails and called onClicked.
+ * If we don't do this, onClicked won't be triggered.
+ */
+
+const forcePopup = ({ id, url }) => {
+  chrome.pageAction.show(id);
+};
+
+chrome.tabs.onUpdated.addListener((tabId, activeInfo, tab) => {
+  forcePopup(tab);
+});
+
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+  chrome.tabs.get(tabId, forcePopup);
+});
+
+chrome.pageAction.onClicked.addListener(({ url }) => {
+  chrome.tabs.executeScript({
+    file: "/remove-script.js",
+    allFrames: true,
   });
 });
+
+// chrome.runtime.onInstalled.addListener(function () {
+//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+//     chrome.declarativeContent.onPageChanged.addRules([
+//       {
+//         conditions: [
+//           new chrome.declarativeContent.PageStateMatcher({
+//             pageUrl: { hostEquals: "" },
+//           }),
+//         ],
+//         actions: [new chrome.declarativeContent.ShowPageAction()],
+//       },
+//     ]);
+//   });
+// });
--- remove-script.js
+++ remove-script.js
@@ -1,7 +1,7 @@
 // remove blurred filter
 document.getElementById("root").style.filter = "none";
 
-// hide the modal
+// hide the "modal"
 document.getElementsByClassName(
   "sc-bdVaJa styled__Wrapper-idlfy4-0 jSiHYb"
 )[0].style.visibility = "hidden";
